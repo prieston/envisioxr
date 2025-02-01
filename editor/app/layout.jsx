84f2576909e2c4a529ae019594f0a18bf35275cb
Theofilos Papadopoulos <priestont@gmail.com>
2025-02-01 02:59:45 +0200

    fix: minor website modifications

--- editor/app/layout.jsx
+++ editor/app/layout.jsx
@@ -1,10 +1,17 @@
 // app/layout.jsx
-"use client";
 
 import "@/global.css";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "@/theme"; // Import the dark theme
 import AdminLayout from "@/components/AdminLayout";
+
+export const metadata = {
+  title: "EnvisioXR | App",
+  description: "This is my cool app.",
+  icons: {
+    icon: "/icons/favicon.ico",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
