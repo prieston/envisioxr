978fb09333ffdcb8e898e770931952a2988e9c66
Theofilos Papadopoulos <priestont@gmail.com>
2025-02-03 20:28:14 +0200

    fix: micro chagne

--- editor/app/utils/toastUtils.js
+++ editor/app/utils/toastUtils.js
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 export const showToast = (message, type = 'info') => {
   toast[type](message, {
     position: 'bottom-right',
-    autoClose: 3000,
+    autoClose: 3500,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
