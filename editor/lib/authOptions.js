ecde9ee0557c6937a6e0f464a44fe9ba70baff15
Theofilos Papadopoulos <priestont@gmail.com>
2025-02-03 21:29:33 +0200

    fix: credentials

--- editor/lib/authOptions.js
+++ editor/lib/authOptions.js
@@ -1,5 +1,4 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import bcrypt from "bcrypt";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
@@ -32,37 +31,6 @@ export const authOptions = {
   },
 
   providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Please enter an email and password");
-        }
-
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-
-        if (!user || !user?.password) {
-          throw new Error("No user found");
-        }
-
-        const passwordMatch = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
-
-        if (!passwordMatch) {
-          throw new Error("Incorrect password");
-        }
-
-        return user;
-      },
-    }),
 
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
