ce74b6bac4b67bdcfab0db67866e71deb756e6ae
Theofilos Papadopoulos <priestont@gmail.com>
2025-02-03 21:28:09 +0200

    fix: credentials provider

--- website/src/lib/authOptions.ts
+++ website/src/lib/authOptions.ts
@@ -1,6 +1,5 @@
 import { prisma } from "@/app/libs/prismaDB";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import bcrypt from "bcrypt";
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GitHubProvider from "next-auth/providers/github";
@@ -33,38 +32,6 @@ export const authOptions: NextAuthOptions = {
   },
 
   providers: [
-    CredentialsProvider({
-      name: "credentials",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Please enter an email and password");
-        }
-
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-
-        if (!user || !user?.password) {
-          throw new Error("No user found");
-        }
-
-        const passwordMatch = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
-
-        if (!passwordMatch) {
-          throw new Error("Incorrect password");
-        }
-
-        return user;
-      },
-    }),
-
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
