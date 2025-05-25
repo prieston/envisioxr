5f05fcd4100820282d515a266d42ebcb98cf50cd
Theofilos Papadopoulos <priestont@gmail.com>
2025-05-25 15:05:09 +0300

    fix: build issues

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -9,6 +9,20 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { _isServer }) => {
+    // Add a rule to handle the undici module
+    config.module.rules.push({
+      test: /node_modules\/undici/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
