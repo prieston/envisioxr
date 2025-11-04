d472238634cf464323616e4c2e81ae3873216ab3
Theofilos Papadopoulos <priestont@gmail.com>
2025-11-04 23:10:38 +0200

    fix: use lazy loading for ion-sdk vendor modules to prevent SSR errors
    - Use Promise-based dynamic imports to load vendor modules only on client
    - Provide SSR-safe stubs that get replaced at runtime
    - Vendor modules execute code that redefines properties, causing SSR failures
    - Adding 'use client' to ObjectPropertiesView ensures component is client-only
    
    The vendor JS files try to redefine 'conicSensor' property during module
    evaluation, which fails during SSR. By lazily loading them only on the
    client side, we prevent this error.

--- packages/ion-sdk/src/index.ts
+++ packages/ion-sdk/src/index.ts
@@ -2,22 +2,40 @@ export * from "./utils/sensors";
 export { default as ViewshedAnalysis } from "./views/ViewshedAnalysis";
 export * from "./lib/init";
 
-// Re-export vendor modules (JS files, available at runtime via dist/vendor)
-// These are JS files excluded from TypeScript compilation but copied to dist
-// Only import/export on client side to prevent SSR errors with property redefinition
+// Vendor modules are loaded lazily to prevent SSR errors
+// The vendor JS files execute code that redefines properties on prototypes
+// This causes "Cannot redefine property" errors during SSR
+// Components importing these should be marked with "use client"
+
+// Stub exports for SSR - will be replaced at runtime on client
+export const TransformEditor = undefined as any;
+export const IonGeometry = {} as any;
+export const IonSensors = {} as any;
+export const IonMeasurements = {} as any;
+
+// Lazy load vendor modules only on client side
 if (typeof window !== "undefined") {
-  // @ts-ignore - Vendor JS files are not type-checked, only copied to dist
-  export { default as TransformEditor } from "./vendor/cesium-ion-sdk/ion-sdk-measurements/Source/TransformEditor/TransformEditor.js";
-  // @ts-ignore
-  export * as IonGeometry from "./vendor/cesium-ion-sdk/ion-sdk-geometry/index.js";
-  // @ts-ignore
-  export * as IonSensors from "./vendor/cesium-ion-sdk/ion-sdk-sensors/index.js";
-  // @ts-ignore
-  export * as IonMeasurements from "./vendor/cesium-ion-sdk/ion-sdk-measurements/index.js";
-} else {
-  // SSR-safe stubs
-  export const TransformEditor = undefined as any;
-  export const IonGeometry = {} as any;
-  export const IonSensors = {} as any;
-  export const IonMeasurements = {} as any;
+  // Use dynamic import to load vendor modules only when needed
+  // This prevents the vendor code from executing during SSR
+  Promise.all([
+    import("./vendor/cesium-ion-sdk/ion-sdk-measurements/Source/TransformEditor/TransformEditor.js").catch(() => null),
+    import("./vendor/cesium-ion-sdk/ion-sdk-geometry/index.js").catch(() => null),
+    import("./vendor/cesium-ion-sdk/ion-sdk-sensors/index.js").catch(() => null),
+    import("./vendor/cesium-ion-sdk/ion-sdk-measurements/index.js").catch(() => null),
+  ]).then(([TransformEditorModule, IonGeometryModule, IonSensorsModule, IonMeasurementsModule]) => {
+    if (TransformEditorModule) {
+      Object.assign(TransformEditor, TransformEditorModule.default || TransformEditorModule);
+    }
+    if (IonGeometryModule) {
+      Object.assign(IonGeometry, IonGeometryModule);
+    }
+    if (IonSensorsModule) {
+      Object.assign(IonSensors, IonSensorsModule);
+    }
+    if (IonMeasurementsModule) {
+      Object.assign(IonMeasurements, IonMeasurementsModule);
+    }
+  }).catch(() => {
+    // Silently fail if vendor modules can't be loaded
+  });
 }
